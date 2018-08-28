88ef8090cc80699b63552c6e7916302b85c4554e
Aderonke <aderonkeakinyemi@gmail.com>
2018-08-28 16:28:21 +0100

    errornous ravenous project

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './SearchBar.css';
+
+
+
+
+
+class SearchBar extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      term:'',
+      location: '',
+      sortBy: 'best_match'
+    };
+
+    const sortByOptions = {
+      'Best Match':'best_match',
+      'Highest Rated':'rating',
+      'Most Reviewed':'review_count'
+    };
+
+    getSortByClass(sortByOption){
+      if(this.state.sortBy === sortByOption){
+        return 'active';
+      } else {
+        return '';
+      }
+    }
+
+
+
+  }
+
+  renderSortByOptions(){
+    return Object.keys(sortByOptions).map(sortByOption => {
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return <li key={sortByOptionValue} className={get}>{sortByOption}</li>;
+    });
+  }
+    render(){
+      return(
+        <div className="SearchBar">
+          <div className="SearchBar-sort-options">
+            <ul>
+            {this.renderSortByOptions()}
+            </ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
+      </div>
+      <div className="SearchBar-submit">
+      <a>Lets Go</a>
+      </div>
+    </div>
+      );
+    }
+
+}
+
+export default SearchBar;
